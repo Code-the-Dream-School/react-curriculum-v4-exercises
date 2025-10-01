1b6c084574fe644c580560d9b54256518143798c
Ines Chavez <ineschavez@iness.macbook.pro.lan>
2025-10-01 18:00:50 -0400

    updating landing page

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./private/pages/landing";
+import Lesson from "./private/pages/lesson";
+
+createRoot(document.getElementById("root")).render(
+
+
+    
+);
+
+
---
+++ src/private/pages/landing.jsx
@@ -0,0 +1,41 @@
+//landing page component with weekly lesson sector
+//main job is to route to weekly lesson pages
+
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
+
+const weeklyLessons = [
+    {slug: 'week-01', title: 'Week 01: Introduction to React'},
+    {slug: 'week-02', title: 'Week 02:'},
+    {slug: 'week-03', title: 'Week 03:'},
+    {slug: 'week-04', title: 'Week 04:'},
+    {slug: 'week-05', title: 'Week 05:'},
+    {slug: 'week-06', title: 'Week 06:'},
+    {slug: 'week-07', title: 'Week 07:'},
+    {slug: 'week-08', title: 'Week 08:'},
+    {slug: 'week-09', title: 'Week 09:'},
+    {slug: 'week-10', title: 'Week 10:'},
+    {slug: 'week-11', title: 'Week 11:'},
+]
+
+export default function Landing() {
+    return (
+        <div>
+        <h1> WELCOME, to our react exercises </h1>
+        <div>
+        <h2> Weekly Lessons: </h2>
+        <ul> 
+        {weeklyLessons.map(lesson => (
+            <li key={lesson.slug}>
+                <Link to={`/lessons/${lesson.slug}`}>{lesson.title}</Link>
+            </li> 
+        ))}
+        </ul>
+
+
+        </div>
+
+        </div>
+    );
+}
