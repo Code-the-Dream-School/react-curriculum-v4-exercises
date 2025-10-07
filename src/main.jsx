1b872beb3c1e85a465cd6aff3f4af1b95ba3f759
Ines Chavez <ineschavez@iness.macbook.pro.lan>
2025-10-06 20:32:31 -0400

    connected landing to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./private/pages/landing";
-import Lesson from "./private/pages/lesson";
+// import lesson from "./private/pages/Lesson";
 
 createRoot(document.getElementById("root")).render(
-
-
+    <BrowserRouter> 
+        <Routes>
+            <Route path="/" element={<Landing />} />
+        </Routes>
+    </BrowserRouter>
     
 );
 
