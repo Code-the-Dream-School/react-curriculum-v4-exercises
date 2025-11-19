c4e11d7637236aa855e8aa27f16be72dd25840a6
Ines Chavez <ineschavez@iness.macbook.pro.lan>
2025-11-18 20:23:49 -0500

    instructions ready for week-02

--- src/exercises/week-02/studentWork.jsx
+++ src/exercises/week-02/studentWork.jsx
@@ -1,4 +1,12 @@
+//Week-02 Building with ReactDOM and components
+//Exercise: Build a "Snack Ranking App" Component in this file
 
-export default function StudentWork({ name }) {
-  return <h1>Hello, {name || 'World'}!</h1>;
+export default function StudentWork() {
+  return(
+    <div>
+      {/* add JSX here */}
+      <p> Student output will go here</p>
+    </div>
+
+  );
 }
