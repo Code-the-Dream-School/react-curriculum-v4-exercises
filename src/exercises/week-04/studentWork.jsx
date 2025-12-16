fde7dbf4a76aa9691a9856b9eef4d498a52fb669
royemosby <roy.e.mosby@gmail.com>
2025-12-16 09:13:43 -0500

    Add exercise component instances

--- src/exercises/week-04/studentWork.jsx
+++ src/exercises/week-04/studentWork.jsx
@@ -1,8 +1,20 @@
-export default function StudentWork() { 
-    return ( 
-        <div> 
-            {/* add components here */} 
-            <p>Student output will go here</p> 
-        </div> 
-    ); 
+import BugStrictMode from './BugStrictMode';
+import FindCorrectHook from './FindCorrectHook';
+import BugEventPropagation from './BugEventPropagation';
+import FillRefFocus from './FillRefFocus';
+import BugChildParent from './BugChildParent';
+export default function StudentWork() {
+  return (
+    <div>
+      <BugStrictMode />
+      <hr />
+      <FindCorrectHook />
+      <hr />
+      <BugEventPropagation />
+      <hr />
+      <FillRefFocus />
+      <hr />
+      <BugChildParent />
+    </div>
+  );
 }
