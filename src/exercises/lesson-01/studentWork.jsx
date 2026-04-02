ab800e89ca69cb04504a234a97930684a52c7d37
waleedtarbosh <waleedtarbush@gmail.com>
2026-04-02 13:15:16 -0400

    Complete Lesson 01: About Me component and screenshot

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,21 @@
 
 export default function StudentWork() {
   //add variables here
+  const myName = 'Waleed Tarbosh';
+  const myAge = 27;
+  const hobbies = ['AI', 'Coding', 'Design', 'Gaming', 'Reading'];
+
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>Hello, I am {myName}</h1>
+      <p>I am {myAge} years old</p>
+      <h3>My Hobbies:</h3>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
