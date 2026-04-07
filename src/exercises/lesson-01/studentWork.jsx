c80ce39ef6269fb45ecbc5b111ef2b5b1d740dba
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-04-07 09:47:18 -0700

    Lesson-01-exercise-done

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,9 +3,28 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = 'Yuhan Kong';
+
+  const age = 32;
+
+  const hobbies = ['Cooking', 'Doing yoga', 'Hiking'];
+
   return (
     <div>
       {/* add JSX here */}
+      <h1>About me</h1>
+      <p>
+        Hi, my name is {name} and I currently live in Seattle.I have a
+        background in finance and previously worked as a clearing accountant,
+        where I collaborated with engineers to improve workflows through
+        automation. That experience sparked my interest in programming. Outside
+        of coding, I enjoy:
+      </p>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
       <p> Student output will go here </p>
     </div>
   );
