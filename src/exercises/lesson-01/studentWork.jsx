d9fec957c6df221b6ef63dbbf279ffe7004dde41
Blanquitta <lilian0317@aol.com>
2026-04-09 22:57:13 -0400

    lesson-01-exercise

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -1,12 +1,34 @@
 //Lesson-01 Introduction to React
 //Exercise: Build an "About Me" Component in this file
 
+import React from 'react';
+
 export default function StudentWork() {
-  //add variables here
+  const information = [
+    { id: 1, title: 'Lilian' },
+    { id: 2, title: '57' },
+  ];
+  const hobbies = [
+    { id: 3, title: 'travel' },
+    { id: 4, title: 'write' },
+  ];
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1> About me</h1>
+      <p> Hi, my name is Lilian and I enyoy learning new things</p>
+
+      <ul>
+        {information.map((information) => (
+          <li key={information.id}>{information.title}</li>
+        ))}
+      </ul>
+
+      <h2> My Hobbies </h2>
+      <ul>
+        {hobbies.map((hobbies) => (
+          <li key={hobbies.id}>{hobbies.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
