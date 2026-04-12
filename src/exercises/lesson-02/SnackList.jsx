66abf456bd96cc24af1f38bc65272147f939a502
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2026-04-12 01:31:57 -0700

    Create SnackList component

---
+++ src/exercises/lesson-02/SnackList.jsx
@@ -0,0 +1,23 @@
+export default function SnackList() {
+  const snacks = [
+    { name: 'Chips,rank: 3' },
+    { name: 'Chocolate', rank: 1 },
+    { name: 'Popcorn', rank: 2 },
+    { name: 'Cookies', rank: 4 },
+  ];
+
+  const sortedSnacks = snacks.toSorted((a, b) => a.rank - b.rank);
+
+  return (
+    <div>
+      <h2>My Favorite Snacks</h2>
+      <o1>
+        {sortedSnacks.map((snack, index) => (
+          <li key={index}>
+            {snack.name} (Rank: {snack.rank})
+          </li>
+        ))}
+      </o1>
+    </div>
+  );
+}
