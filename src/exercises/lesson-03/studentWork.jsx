bf50b9a51ebd15e6fb0cccb2364f948f5ded652a
Coding <codingmaniac227@gmail.com>
2026-04-18 09:48:39 -0400

    refactor(studentWork): move fixed bugs into parent component

--- src/exercises/lesson-03/studentWork.jsx
+++ src/exercises/lesson-03/studentWork.jsx
@@ -2,11 +2,17 @@
 //Exercise: React Bug Hunt – Fix the broken components in this folder
 //Impport components here
 
+import BugProps from './BugProps.jsx';
+import BugEffectLoop from './BugEffectLoop.jsx';
+import BugMutatedState from './BugMutatedState.jsx';
+
 export default function StudentWork() {
   return (
     <div>
       {/* add components here */}
-      <p>Student output will go here</p>
+      <BugEffectLoop />
+      <BugMutatedState />
+      <BugProps />
     </div>
   );
 }
