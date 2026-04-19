e78f42bd9f9e84074902085b4fb8a748b5d8a1f9
Coding <codingmaniac227@gmail.com>
2026-04-19 11:13:29 -0400

    refactor(Child): utilize handleIncrement helper from Parent in order to attach it to Child button

--- src/exercises/lesson-04/BugChildParentCommuncation/Child.jsx
+++ src/exercises/lesson-04/BugChildParentCommuncation/Child.jsx
@@ -1,3 +1,3 @@
-export default function Child() {
-  return <button onClick={() => {}}>Increment Counter</button>;
+export default function Child({ handleIncrement }) {
+  return <button onClick={handleIncrement}>Increment Counter</button>;
 }
