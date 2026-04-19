45862d0bb4461c058876b8390bab8e3621e271d7
Coding <codingmaniac227@gmail.com>
2026-04-19 10:48:24 -0400

    refactor(FindCorrectHook): manually manipulate DOM via useRef hook in order to update counter

--- src/exercises/lesson-04/FindCorrectHook.jsx
+++ src/exercises/lesson-04/FindCorrectHook.jsx
@@ -1,16 +1,23 @@
 // TOPIC: Choose the correct tool: useRef vs useState
 // TASK: Make sure it updates the text *without* triggering a re-render
+import React from 'react';
+
 export default function FindCorrectHook() {
-  let clickCount = 0; // ← incorrect implementation
+  let clickCount = React.useRef(0);
+  let buttonRef = React.useRef(null);
 
   function handleClick() {
-    clickCount++;
+    clickCount.current += 1;
+    buttonRef.current.innerText = `${clickCount.current} Clicks`;
+    console.log(clickCount.current);
   }
 
   return (
     <div>
       <h2>useRef vs useState Decision</h2>
-      <button onClick={handleClick}>{clickCount} Clicks</button>
+      <button onClick={handleClick} ref={buttonRef}>
+        0 Clicks
+      </button>
     </div>
   );
 }
