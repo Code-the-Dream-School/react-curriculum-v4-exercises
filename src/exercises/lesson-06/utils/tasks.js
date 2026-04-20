70cf6944395f617fa37bace8b3da344f479e2fc9
Coding <codingmaniac227@gmail.com>
2026-04-20 05:20:05 -0400

    feat(tasks): create utility vanilla js file that holds tasks objects

---
+++ src/exercises/lesson-06/utils/tasks.js
@@ -0,0 +1,5 @@
+export const tasks = [
+  { id: 1, title: 'Learn React', completed: true },
+  { id: 2, title: 'Refactor code', completed: false },
+  { id: 3, title: 'Organize files', completed: false },
+];
