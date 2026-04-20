7069ce6b3aae206c9eeb1a8c0aaeb4921535fa31
Yuhan-Kong <kong-sky@163.com>
2026-04-20 16:23:53 -0700

    fix BugProps using state instead of local variable

--- src/exercises/lesson-03/BugProps.jsx
+++ src/exercises/lesson-03/BugProps.jsx
@@ -11,12 +11,12 @@
 
   Use the commented "Explanation" section at the bottom of this lesson's components.
 */
-
+import { useState } from 'react';
 export default function BugProps({ name = 'friend' }) {
-  let message = 'Hello, ' + name;
+  const [message, setMessage] = useState('Hello, ' + name);
 
   function handleChange() {
-    message = 'Hi, ' + name + '!';
+    setMessage('Hi, ' + name + '!');
   }
 
   return (
