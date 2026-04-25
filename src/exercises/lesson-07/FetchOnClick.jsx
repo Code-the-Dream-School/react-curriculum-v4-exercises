c3d4be3389f9d3067e89eba0db934a61bf47e6f0
Coding <codingmaniac227@gmail.com>
2026-04-25 13:13:15 -0400

    refactor(FetchOnClick): create handler function and loading states for button click events to fetch random posts from api

--- src/exercises/lesson-07/FetchOnClick.jsx
+++ src/exercises/lesson-07/FetchOnClick.jsx
@@ -1,13 +1,42 @@
 import './Lesson07Styles.css';
+import { getSinglePost } from './api';
+import PostsList from './components/PostsList';
+import { useState } from 'react';
 
 export default function FetchOnClick() {
+  const [singlePost, setSinglePost] = useState([]);
+  const [loadingError, setLoadingError] = useState(false);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleFetchedData = async () => {
+    try {
+      setLoading(true);
+      const data = await getSinglePost(Math.floor(Math.random() * 101) + 1);
+      setSinglePost([data]);
+    } catch (err) {
+      setLoadingError(true);
+      setLoading(false);
+      setError(`Error: ${err.message}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="root">
       <h1 className="heading">Fetch single post on click</h1>
-      <button type="button">Get post</button>
+      <button type="button" onClick={handleFetchedData}>
+        Get post
+      </button>
       <div className="content">
-        TODO: Replace me with fetched data when the <code>Get post</code> button
-        is clicked
+        {loadingError && (
+          <p>
+            <strong>{error}</strong>
+          </p>
+        )}
+        {loading && <p>loading...</p>}
+        {!loadingError && !loading && <PostsList posts={singlePost} />}
       </div>
     </div>
   );
