efac37a4113f9345145855cd41bea4b82a4c57da
Coding <codingmaniac227@gmail.com>
2026-04-25 13:20:28 -0400

    refactor(FetchOnRender): create useEffect hook that fetches data and renders it based on loading state

--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -1,11 +1,42 @@
 import './Lesson07Styles.css';
+import { getPosts } from './api.js';
+import React, { useEffect } from 'react';
+import PostsList from './components/PostsList.jsx';
 
 export default function FetchOnRender() {
+  const [posts, setPosts] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [loadingError, setLoadingError] = React.useState(false);
+  const [error, setError] = React.useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const data = await getPosts();
+        setPosts(data);
+      } catch (err) {
+        setError(`Error: ${err.message}`);
+        setLoading(false);
+        setLoadingError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="root">
       <h1 className="heading">Fetch list of posts on render</h1>
       <div className="content">
-        TODO: Replace me with fetched data when the component renders
+        {loadingError && (
+          <p>
+            <strong>{error}</strong>
+          </p>
+        )}
+        {loading && <p>loading...</p>}
+        {!loadingError && !loading && <PostsList posts={posts} />}
       </div>
     </div>
   );
