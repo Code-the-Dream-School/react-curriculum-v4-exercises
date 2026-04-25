1e443ad36c4e9046adf3063b55e3f36824f22336
Coding <codingmaniac227@gmail.com>
2026-04-25 13:33:39 -0400

    refactor(api): refactor exported functions into asynchronous functions in order to handle data fetching properly without running into synchronous issues on a single-threaded js environment

--- src/exercises/lesson-07/api.js
+++ src/exercises/lesson-07/api.js
@@ -22,14 +22,15 @@ const POSTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts/';
  * - title
  * - body
  */
-export function getPosts() {
+export async function getPosts() {
   console.log('[getPosts]: fetching list of posts');
+  const url = `${POSTS_ENDPOINT}?_limit=10`;
 
-  // TODO: use this `url` const to fetch the list of posts
-  // and return some JSON data.
-  // You may delete this comment once you've finished the implementation.
-  // eslint-disable-next-line no-unused-vars
-  const url = POSTS_ENDPOINT;
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error('Failed to fetch posts');
+  }
+  return await res.json();
 }
 
 /**
@@ -39,16 +40,18 @@ export function getPosts() {
  * - title
  * - body
  */
-export function getSinglePost(postId) {
+export async function getSinglePost(postId) {
   if (!postId) {
     throw new Error('[getSinglePost]: postId parameter is required!');
   }
 
   console.log('[getSinglePost]: fetching post with id:', postId);
-
-  // TODO: use this `url` const to fetch the single post
-  // and return some JSON data.
-  // You may delete this comment once you've finished the implementation.
-  // eslint-disable-next-line no-unused-vars
   const url = `${POSTS_ENDPOINT}${postId}`;
+
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error('Failed to fetch single post');
+  }
+
+  return await res.json();
 }
