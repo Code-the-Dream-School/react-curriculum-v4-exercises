4d14bacb41c01d6c2f30af82d8852de2c62df8b7
Yuhan-Kong <kong-sky@163.com>
2026-05-12 12:42:08 -0700

    Import reusable components

--- src/exercises/lesson-06/studentWork.jsx
+++ src/exercises/lesson-06/studentWork.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from 'react';
+import UserProfile from '../../components/UserProfile';
+import TaskFilter from '../../components/TaskFilter';
+import TaskItem from '../../components/TaskItem';
 
 export default function StudentWork() {
   const [tasks, setTasks] = useState([]);
