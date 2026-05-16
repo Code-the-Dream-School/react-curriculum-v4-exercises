e131d9bf4e332dc7fc3d6a260c315c8245cd3c09
ebordenave <edward.bordenave@gmail.com>
2026-05-16 14:40:20 -0400

    complete getPosts() and getSinglePost()

--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from 'react';
 import './Lesson07Styles.css';
+import { getPosts } from './api';
 
 export default function FetchOnRender() {
+  // When the component renders, call getPosts().
+
+  const [posts, setPosts] = useState([]);
+  // const [error, setError] = useState('');
+
+  useEffect(() => {
+    async function startFetching() {
+      const result = await getPosts();
+      setPosts(result);
+    }
+    startFetching();
+  }, []);
+
   return (
     <div className="root">
-      <h1 className="heading">Fetch list of posts on render</h1>
+      <h1 className="heading">
+        Fetch list of posts on render
+      </h1>
       <div className="content">
-        TODO: Replace me with fetched data when the component renders
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
 }
+// Fetching data on render
+// In FetchOnRender.jsx, import the getPosts() function from src/exercises/lesson-07/api.js.
+// Note: This function doesn't do anything yet! You need to finish the logic to make a fetch request in src/exercises/lesson-07/api.js.
+// Once getPosts() returns data, display the list of posts in the component.
+// Each post should display its title and body. Render these in an <h2> and a <p>, respectively.
