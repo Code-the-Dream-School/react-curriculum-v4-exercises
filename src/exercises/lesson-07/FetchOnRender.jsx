3eaed2af1f424ceb513cd40213a9a6d926c52ef1
Yuhan-Kong <kong-sky@163.com>
2026-05-19 16:13:20 -0700

    repalced content to posts.map

--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -14,7 +14,9 @@ export default function FetchOnRender() {
     <div className="root">
       <h1 className="heading">Fetch list of posts on render</h1>
       <div className="content">
-        TODO: Replace me with fetched data when the component renders
+        {posts.map((post) => (
+          <div key={post.id}></div>
+        ))}
       </div>
     </div>
   );
