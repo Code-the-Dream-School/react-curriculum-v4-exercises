56f53461b3dd8203d001c96be7e1480636ecc978
Yuhan-Kong <kong-sky@163.com>
2026-05-19 16:42:51 -0700

    getPosts display title and body

--- src/exercises/lesson-07/FetchOnClick.jsx
+++ src/exercises/lesson-07/FetchOnClick.jsx
@@ -13,6 +13,12 @@ export default function FetchOnClick() {
       <h1 className="heading">Fetch single post on click</h1>
       <button type="button">Get post</button>
       <div className="content">
+        {post && (
+          <div>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        )}
         <button onClick={getPost}>Get post</button>
       </div>
     </div>
