2d63cef14b2e0381ea017717e827ac9e28826591
Yuhan-Kong <kong-sky@163.com>
2026-05-19 16:52:44 -0700

    Limit the results of getPosts function to 10 posts for stretch goals

--- src/exercises/lesson-07/api.js
+++ src/exercises/lesson-07/api.js
@@ -1,31 +1,9 @@
 const POSTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts/';
 
-/**
- * Instructions:
- * The `POSTS_ENDPOINT` url returns a list of post objects
- * from the JSONPlaceholder API.
- * Your task is to finish implementing the two functions in this file
- * so that they fetch data from the API and return the results as JSON.
- * Then, you will use that data in `<FetchOnRender>` and `<FetchOnClick>`.
- *
- * TIP: A request to `https://jsonplaceholder.typicode.com/posts/`
- * returns an array of posts.
- * A request to `https://jsonplaceholder.typicode.com/posts/1`
- * returns a single post object with an `id` of 1.
- * Try pasting those URLs into your browser to see the results!
- */
-
-/**
- * Should return an array of posts with the following properties:
- * - userId
- * - id
- * - title
- * - body
- */
 export function getPosts() {
   console.log('[getPosts]: fetching list of posts');
 
-  const url = POSTS_ENDPOINT;
+  const url = `${POSTS_ENDPOINT}?_limit=10`;
 
   return fetch(url).then((response) => {
     return response.json();
