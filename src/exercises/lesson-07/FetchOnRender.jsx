032ac6d54c29a24114fa2d0e66ba73cbe1a84112
Yuhan-Kong <kong-sky@163.com>
2026-05-19 18:24:37 -0700

    Add loading display

--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -21,6 +21,7 @@ export default function FetchOnRender() {
     <div className="root">
       <h1 className="heading">Fetch list of posts on render</h1>
       <div className="content">
+        {isLoading && <p>Loading</p>}
         {posts.map((post) => (
           <div key={post.id}>
             <h2>{post.title}</h2>
