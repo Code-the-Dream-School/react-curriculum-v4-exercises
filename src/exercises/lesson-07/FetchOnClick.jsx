0f73c9288bfcef51e99b3bb65512238d34da1aef
kelliwisethebrave <198285550+kelliwisethebrave@users.noreply.github.com>
2026-05-25 16:57:11 -0600

    Update to complete required steps, FetchOnRender and FetchOnClick updated as well as api.js to fetch post(s) as required

--- src/exercises/lesson-07/FetchOnClick.jsx
+++ src/exercises/lesson-07/FetchOnClick.jsx
@@ -1,13 +1,25 @@
 import './Lesson07Styles.css';
+import { getSinglePost } from './api.js';
+import { useState } from 'react';
 
 export default function FetchOnClick() {
+  const [post, setPost] = useState('');
+
+  async function fetchPost() {
+    const number = Math.floor(Math.random() * 100);
+    console.log(number);
+    const post = await getSinglePost(number);
+    setPost(post);
+  }
   return (
     <div className="root">
       <h1 className="heading">Fetch single post on click</h1>
-      <button type="button">Get post</button>
+      <button type="button" onClick={fetchPost}>
+        Get post
+      </button>
       <div className="content">
-        TODO: Replace me with fetched data when the <code>Get post</code> button
-        is clicked
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
       </div>
     </div>
   );
