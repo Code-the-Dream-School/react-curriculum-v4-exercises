cd95a8f3a16bcbf90de12c5e6bfe90ae3e5efbf2
Yuhan-Kong <kong-sky@163.com>
2026-06-09 16:33:08 -0700

    Debug:add a navigation link that routes to /products/<product-id>

--- src/exercises/lesson-10/pages/Home.jsx
+++ src/exercises/lesson-10/pages/Home.jsx
@@ -41,6 +41,13 @@ export default function Home({ products }) {
             <p style={{ margin: 0 }}>
               <strong>${p.price.toFixed(2)}</strong>
             </p>
+
+            <Link
+              to={`/products/${p.id}`}
+              style={{ marginTop: 8, display: 'inline-block' }}
+            >
+              View Details
+            </Link>
           </article>
         ))}
       </div>
