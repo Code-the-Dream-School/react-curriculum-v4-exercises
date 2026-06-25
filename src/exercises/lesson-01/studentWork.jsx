4714397ad0654453cdf7603ed93fc1a16e5c625a
AnnnAPr <programming.aap@gmail.com>
2026-06-24 19:27:14 -1000

    Add about me

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,24 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = 'Anya';
+  const age = 30;
+  const hobbies = ['reading', 'hiking', 'cooking'];
+
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me</h1>
+      <p>
+        {' '}
+        I am {name} and I am {age} years old.{' '}
+      </p>
+      <h2>My hobbies are:</h2>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
